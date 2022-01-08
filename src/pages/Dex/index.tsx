cf4d243e4753e7edf13e84ae2b536cc0f70e6831
Arian Derida <arianderida@gmail.com>
2022-01-09 00:22:17 +0700

    owned feature pomeon

--- src/pages/Dex/index.tsx
+++ src/pages/Dex/index.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DexComponent from "../../components/Dex";
+import { deleteCookies, getCookies, setCookies } from "../../libs";
+import { ICookies } from "../../libs/interface";
 
 const DexPage = () => {
-  return <DexComponent />;
+  const [pokemons, setPokemons] = useState<Array<ICookies> | null>(null);
+
+  useEffect(() => {
+    GetPokemon();
+  }, []);
+
+  const GetPokemon = async () => {
+    const temporary = await getCookies("__UUPK");
+
+    if (temporary) {
+      setPokemons(JSON.parse(temporary));
+    }
+  };
+
+  const onRelease = async (index: number) => {
+    const temporary = JSON.parse(JSON.stringify(pokemons));
+    temporary.splice(index, 1);
+
+    await setCookies("__UUPK", JSON.stringify(temporary));
+
+    if (pokemons?.length === 1) {
+      setPokemons(null);
+      deleteCookies("__UUPK");
+    } else {
+      await GetPokemon();
+    }
+  };
+
+  return <DexComponent props={{ pokemons, onRelease }} />;
 };
 
 export default DexPage;
